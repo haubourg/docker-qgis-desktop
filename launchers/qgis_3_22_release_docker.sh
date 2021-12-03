ac4591a053052e014085096ed9f1136ab21d9dc1
Régis Haubourg <regis.haubourg@cstb.fr>
2021-12-03 16:00:16 +0100

    add launcher template file

---
+++ launchers/qgis_3_22_release_docker.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Should be platform neutral - at least working on Linux and Windows
+USER_NAME=`basename $HOME`
+
+# HHHOME is used to pass the HOME directory of the user running qgis
+# and is used in "start.sh" to create the same user within the container.
+
+# Users home is mounted as home
+# --rm will remove the container as soon as it ends
+
+
+docker run --rm \
+    -i -t \
+    -v ${HOME}:/home/${USER_NAME} \
+    -v /data:/data \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=unix$DISPLAY \
+    -e HHHOME=${HOME} \
+    -e PGSERVICEFILE=${HOME}/.pg_service.conf \
+    --net=host --privileged \
+    qgis_local:3.22
+    
+    #  qgis_local:3.14 --profiles-path  ${HOME}/OSLANDIA/support/gendarmerie_stsi2/profil_dev $*
+
+
+# you can change this last line to pass  startup options to QGIS
+# If you want to use another location for qgis profile, you can do this for instance, using a .qgis2_docker directory instead of the default .qgis2 :
+#  jancelindocker-qgis-desktop:2.18 /start.sh --configpath /home/${USER_NAME}/.qgis2_docker
