a5586e9031d1ef34b4f6662d4d5980cb0ccbd97e
Régis Haubourg <regis.haubourg@cstb.fr>
2023-03-10 10:27:19 +0100

    switch to 3.30

--- start_qgis.sh
+++ start_qgis.sh
@@ -20,7 +20,7 @@ docker run --rm \
     -e HHHOME=${HOME} \
     -e PGSERVICEFILE=${HOME}/.pg_service.conf \
     --net=host --privileged \
-    ghcr.io/haubourg/qgis_local:3.28
+    ghcr.io/haubourg/qgis_local:3.30
     
     #--configpath /home/${USER_NAME}/.local/share/QGIS/QGIS3/profiles/
 
